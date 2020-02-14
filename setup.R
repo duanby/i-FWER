1c8f3dbac4f18c83271599245c31f9fb094c6e57
Boyan Duan <boyanduan@gmail.com>
2020-02-14 09:20:07 -0500

    change setup

--- setup.R
+++ setup.R
@@ -3,11 +3,12 @@ source("sim_dat.R")
 source("i-FWER.R")
 source("single_experiment.R")
 
-load_silently = function(packages){
-  for(package in packages){
-    suppressPackageStartupMessages(library(package, character.only = TRUE))    
-  }
-  invisible()
-}
-load_silently(c("magrittr", "splines", "robustbase", "ggplot2"))
+
+suppressPackageStartupMessages({
+  library(magrittr)
+  library(splines)
+  library(robustbase)
+  library(ggplot2)
+})
+
 
